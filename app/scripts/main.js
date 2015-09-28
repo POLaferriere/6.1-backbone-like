d0814bd58f3a525b19c9d825be7d4fa159cb50c7
Pierre Laferriere <polaferriere@gmail.com>
2015-09-28 15:07:40 -0400

    Completed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,24 @@
+var LikeButton = require('models/like');
+
+
 $(document).ready(function(){
   // prepend the contents of `app/templates/application.hbs` into `body`
   $('#container').append(JST.application());
+
+  var likeButton = new LikeButton({
+    likes: 0
+  });
+
+  $('.like').on('click', function(){
+    likeButton.like();
+  });
+
+  likeButton.on('change:likes', function(){
+    var likes = likeButton.get('likes');
+    if(likeButton.toJSON() === 1){
+      $('.like').text(likes + ' Like');
+    } else {
+      $('.like').text(likes + ' Likes');
+    }
+  });
 });
